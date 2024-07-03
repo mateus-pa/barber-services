0a9c8d0205de84ccb038e4a42d043e584da01c44
mateus-pa <mateuspinheiro1410@hotmail.com>
2024-07-02 22:14:55 -0300

    refactor: delete app service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from "@nestjs/common";
-import { AppService } from "./app.service";
 import { PrismaModule } from "./database/prisma.module";
 import { ExpertsModule } from './experts/experts.module';
 
 @Module({
 	imports: [PrismaModule, ExpertsModule],
 	controllers: [],
-	providers: [AppService]
+	providers: []
 })
 export class AppModule {}
